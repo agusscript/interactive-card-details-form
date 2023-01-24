d1fef139d389d841c7f913aa3f5237c136abcca2
Agustin Sanchez <agus.sanchez.240@gmail.com>
2023-01-24 11:22:33 -0300

    Add manageError and validateForm functional functions

--- script.js
+++ script.js
@@ -1,29 +1,16 @@
-const $form = document.querySelector(".card-form");
-const $inputName = document.querySelector("#name");
-const $inputcardNumber = document.querySelector("#card-number");
-const $inputYY = document.querySelector("#YY");
-const $inputMM = document.querySelector("#MM");
-const $inputCVC = document.querySelector("#CVC");
-const userName = $inputName.value;
-const cardNumber = $inputcardNumber.value;
-const YY = $inputYY.value;
-const MM = $inputMM.value;
-const CVC = $inputCVC.value;
-
-function validateForm(event) {
-  const $submittedStatus = document.querySelector(".submitted-status");
-  // $form.classList.add("occult");
-  // $submittedStatus.classList.remove("occult");
-
-  event.preventDefault();
-}
+const $form = document.querySelector("#card-form");
+const userName = $form.name.value;
+const cardNumber = $form["card-number"].value;
+const YY = $form.YY.value;
+const MM = $form.MM.value;
+const CVC = $form.CVC.value;
 
 function validateName(userName) {
-  if(userName.length === 0) {
+  if (userName.length === 0) {
     return "This field cannot be blank";
-  } else if(userName.length > 50) {
+  } else if (userName.length > 50) {
     return "This field cannot contain more than 50 characters";
-  } else if(!/^[ a-z]+$/i.test(userName)) {
+  } else if (!/^[ a-z]+$/i.test(userName)) {
     return "This field can only contain letters";
   } else {
     return "";
@@ -31,9 +18,9 @@ function validateName(userName) {
 }
 
 function validateCardNumber(cardNumber) {
-  if(cardNumber.length === 0) {
+  if (cardNumber.length === 0) {
     return "This field cannot be blank";
-  } else if(!/([0-9]{4}\s?){4}/.test(cardNumber)){
+  } else if (!/([0-9]{4}\s?){4}/.test(cardNumber)) {
     return "Invalid credit card number";
   } else {
     return "";
@@ -41,7 +28,7 @@ function validateCardNumber(cardNumber) {
 }
 
 function validateYear(YY) {
-  if(YY.length === 0) {
+  if (YY.length === 0) {
     return "This field cannot be blank";
   } else if (!/^[0-9][0-9]$/.test(YY)) {
     return "Invalid year format";
@@ -51,7 +38,7 @@ function validateYear(YY) {
 }
 
 function validateMonth(MM) {
-  if(MM.length === 0) {
+  if (MM.length === 0) {
     return "This field cannot be blank";
   } else if (!/^0[0-9]|1[0-2]$/.test(MM)) {
     return "Invalid month format";
@@ -61,13 +48,68 @@ function validateMonth(MM) {
 }
 
 function validateCVC(CVC) {
-  if(CVC.length === 0) {
+  if (CVC.length === 0) {
     return "This field cannot be blank";
   } else if (!/^[0-9][0-9][0-9]$/.test(CVC)) {
     return "Invalid CVC format";
   } else {
     return "";
   }
+}
+
+function validateForm(event) {
+  const $submittedStatus = document.querySelector(".submitted-status");
+  const userName = $form.name.value;
+  const cardNumber = $form["card-number"].value;
+  const YY = $form.YY.value;
+  const MM = $form.MM.value;
+  const CVC = $form.CVC.value;
+
+  const errorName = validateName(userName);
+  const errorCardNumber = validateCardNumber(cardNumber);
+  const errorYear = validateYear(YY);
+  const errorMonth = validateMonth(MM);
+  const errorCVC = validateCVC(CVC);
+
+  const errors = {
+    name: errorName,
+    "card-number": errorCardNumber,
+    YY: errorYear,
+    MM: errorMonth,
+    CVC: errorCVC,
+  };
+
+  const noErrors = manageErrors(errors) === 0;
+
+  if (noErrors) {
+    $form.classList.add("occult");
+    $submittedStatus.classList.remove("occult");
+  }
+
+  event.preventDefault();
+}
+
+function manageErrors(errors) {
+  const keys = Object.keys(errors);
+  let numberOfErrors = 0;
+  const $errorTextContainer = document.querySelector("#errorsText");
+
+  keys.forEach(function (key) {
+    const error = errors[key];
+
+    if (error) {
+      numberOfErrors++;
+      $form[key].className = "error";
+
+      const errorText = document.createElement("li");
+      errorText.textContent = error;
+      $errorTextContainer.appendChild(errorText);
+    } else {
+      $form[key].className = "";
+    }
+  });
+
+  return numberOfErrors;
 }
 
 $form.onsubmit = validateForm;

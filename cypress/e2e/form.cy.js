29450b69f649cbf34e8c65000fc5239fe2b553e7
Agustin Sanchez <agus.sanchez.240@gmail.com>
2023-04-07 20:11:50 -0300

    Add tests to verify that the main page items are showing

--- cypress/e2e/form.cy.js
+++ cypress/e2e/form.cy.js
@@ -1,5 +1,17 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+/// <reference types="Cypress"/>
+
+describe("Card form tests", () => {
+  before(() => {
+    cy.visit("index.html");
+  });
+
+  it("Verify that the main page items are showing", () => {
+    cy.get(".front").should("be.visible");
+    cy.get(".back").should("be.visible");
+    cy.get("#card-form").should("be.visible");
+    
+    cy.get(".bg-mobile").should("not.be.visible");
+    cy.get(".submitted-status").should("not.be.visible");
+  });
+
+});
